a33d8ac108fabe0cfec66577688cdfbad962e83c
Ahmed Sayed Abdelsalam <e.eng.ahmedsayed@gmail.com>
2023-09-16 23:23:06 +0300

    progress

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,12 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  sides_arr = [a,b,c]
+  raise TriangleError if sides_arr.min <= 0
+  raise TriangleError if sides_arr.sort[0,2].sum <= sides_arr.max
+
+  how_many_eaual = sides_arr.tally.values.max
+  {2 => :isosceles, 3 => :equilateral}.fetch(how_many_eaual, :scalene)
 end
 
 # Error class used in part 2.  No need to change this code.
